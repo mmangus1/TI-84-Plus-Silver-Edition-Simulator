1d9a841a7fcd76b338975991f27ae2616ba861cf
mmangus1 <103114956+mmangus1@users.noreply.github.com>
2022-04-06 07:11:28 -0400

    Add files via upload

---
+++ TI-84 Plus Silver Edition.js
@@ -0,0 +1,222 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var alphaonoff = "Off"; // a alphaonoff value of 1 is for alpha key not being active, alphaonoff value of 2 is for alpha active - not locked, alphaonoff value of 3 is alpha key active and locked.
+var secondonoff = "Off"; // value of "Off" is second key not active, value of "On" means second key is active
+var screencommands = ""; // Outputs to sreen as text, I set this to blank so nothing is on screen at start.
+var javascriptcommands = ""; // a string meant to hold data and be excuted to by the eval function to return the answers.
+var value = ""; // string that changes the execution based on buttons pushed.
+
+//TODO: Reposition canvas correctly, so when ran buttons work.
+//TODO: Find working way to make cursor blink on/off.
+
+function ScreenOutput(value){
+    
+    //Alpha Key On, Second Key Off
+    if ((alphaonoff === "On") && (secondonoff === "On")){
+        
+        switch(value){
+            case "undefined":
+                value="";
+                break;
+                
+            //CATALOG Function    
+            case "0":
+                
+                
+        }
+        
+    }
+    
+    
+    //Alpha Key On, Second Key Off
+    if ((alphaonoff === "On") && (secondonoff === "Off")) {
+    }
+    
+    //Second Key On, Alpha Key Off
+    if ((secondonoff === "On") && (alphaonoff === "Off")){
+        
+    }
+    //Second and Alpha Keys both Off or both On
+    if ((secondonoff === "Off") && (alphaonoff === "Off")){
+        
+        // Take value variable that corresponds to keys and make the buttons do as they should.
+        switch(value){
+            
+        // 
+        case "undefined":
+            value = "";
+            break;
+        case "0":
+            screencommands+="0";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="0";
+            break;
+        case "1":
+            screencommands+="1";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="1";
+            break;
+        case "2":
+            screencommands+="2";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="2";
+            break;
+        case "3":
+            screencommands+="3";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="3";
+            break;
+        case "4":
+            screencommands+="4";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="4";
+            break;
+        case "5":
+            screencommands+="5";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="5";
+            break;
+        case "6":
+            screencommands+="6";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="6";
+            break;
+        case "7":
+            screencommands+="7";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="7";
+            break;
+        case "8":
+            screencommands+="8";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="8";
+            break;
+        case "9":
+            screencommands+="9";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="9";
+            break;
+        case "+":
+            screencommands+="+";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="+";
+            break;
+        case "-":
+            screencommands+="-";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="-";
+            break;
+        case "*":
+            screencommands+="*";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="*";
+            break;
+        case "/":
+            screencommands+="/";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="/";
+            break;
+            
+        // TODO: Have to add a history function by saving the last few values in strings from the javascriptcommands string. Have to find how many are saved in hisrtory also.
+        case "CLEAR":
+            screencommands="";
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands="";
+            break;
+        case "**":
+            screencommands+="^"; // TODO: find the appropriate power to text for the screen.
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="**";
+            break;
+            
+        // TODO: Put answer to problem right aligned and one line down.
+        case "ENTER":
+            document.getElementById("ScreenOutput").innerHTML = (eval(javascriptcommands));
+            break;
+        case "2nd":
+            if(secondonoff === "Off"){
+                secondonoff = "On";
+            }
+            else{
+                secondonoff = "Off";
+            }
+            break;
+        case "ALPHA":
+            if(alphaonoff === "Off"){
+                alphaonoff = "On";
+            }
+            else{
+                alphaonoff = "Off";
+            }
+            break;
+        case "GRAPH":
+            break;
+        case "MATH":
+            break;
+        case "POWERNEGONE":
+            screencommands+="^-1"; // TODO: find appropriate symbol for text on screen.
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="**-1";
+            break;
+        case "SECPOW":
+            screencommands+="^2"; // TODO: find appropriate symbol for text on screen.
+            document.getElementById("ScreenOutput").innerHTML = screencommands;
+            javascriptcommands+="**2";
+            break;
+        case "LOG":
+            break;
+        case "LN":
+            break;
+        case "STO":
+            break;
+        case "ONOFF":
+            break;
+        case "MODE":
+            break;
+        case "APPS":
+            break;
+        case "SIN":
+            break;
+        case "COMMA":
+            break;
+        case "DEL":
+            break;
+        case "STAT":
+            break;
+        case "PGRM":
+            break;
+        case "COS":
+            break;
+        case "(":
+            break;
+        case ".":
+            break;
+        case "VARS":
+            break;
+        case "TAN":
+            break;
+        case ")":
+            break;
+        case "UP":
+            break;
+        case "DOWN":
+            break;
+        case "RIGHT":
+            break;
+        case "LEFT":
+            break;
+        case "Y=":
+            break;
+        case "WIN":
+            break;
+        case "ZOOM":
+            break;
+        case "TRACE":
+            break;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+        <script type="text/javascript">
+            
+            <!--
+
+            function TI84Sim() {
+
+                window.open( "TI-84 Plus Silver Edition.html", "TI84Sim", "status = 1, height = 225, width = 225, resizable = 0" );
+
+            }
+
+            //-->
+        </script>
+    </head>
+    <body>
+        <form> <input type="button" onClick="TI84Sim()" value="TI-84 Plus Silver Edition Simulator"> </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
